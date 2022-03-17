086a81701c5bc1f7fff4d91ec2e68c252aacf684
hari-ls <hari@logicsync.com.au>
2022-03-17 23:41:43 +1030

    Added helper and basic routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,35 @@
 const notes = require("express").Router();
+const {
+  readFromFile,
+  writeToFile,
+  readAndAppend,
+} = require("../helpers/fsUtils");
+const file = "./db/db.json";
 
 notes.get("/", (req, res) => {
-  res.json("Static response");
+  readFromFile(file).then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get("/:num", (req, res) => {
+  const num = req.params.num;
+  readFromFile(file)
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json[num - 1];
+      return result ? res.json(result) : res.json("No note found");
+    });
+});
+
+notes.post("/", (req, res) => {
+  res.json("POST");
+});
+
+notes.put("/", (req, res) => {
+  res.json("PUT");
+});
+
+notes.delete("/:id", (req, res) => {
+  res.json("DELETE");
 });
 
 module.exports = notes;
